13c74e2649086a59df402bd3a8fae4fbc0c27adb
Kerr Wall <pkerrwall@gmail.com>
2024-07-06 11:41:26 -0500

    added arguments for dec2_fly_2024

--- scripts/RunDeSeq2_with_args.R
+++ scripts/RunDeSeq2_with_args.R
@@ -9,20 +9,30 @@ library(DESeq2)
 ###################### load the raw count matrix #######################
 firstC <- args[1] # N2 Spin SVIP
 SecondC <- args[2] # N2 Spin SVIP
-Method <- args[3] # salmon | hisat2
-infile <- args[4] # salmon.gene_counts.tsv
-anno_file <- args[5] # sample_info.txt
-outdir <- './'
+infile <- args[3] # salmon.gene_counts.tsv or *.csv
+anno_file <- args[4] # sample_info.txt
+outdir <- args[5] # './'
 
 # read input file
-rawcount<-read.table(file=infile, header=TRUE, sep="\t",  row.names=1, check.names=FALSE, stringsAsFactors=FALSE)
+# check if infile is csv or tsv
+if (grepl("csv", infile)) {
+    rawcount<-read.csv(file=infile, header=TRUE, sep=",",  row.names=1, check.names=FALSE, stringsAsFactors=FALSE)
+} else {
+    rawcount<-read.table(file=infile, header=TRUE, sep="\t",  row.names=1, check.names=FALSE, stringsAsFactors=FALSE)
+}
 print(head(rawcount))
 
 # convert to integer
 rawcount[, 1:ncol(rawcount)] <- sapply(rawcount[, 1:ncol(rawcount)], as.integer)
+print(head(rawcount))
 
 ###################### Data annotation  #################################
-anno <-read.table(anno_file,header=TRUE,  sep="\t") ##In this case Two coulmns (a) sample (b) Condition
+# check if annotation file is csv or tsv ##In this case Two coulmns (a) sample (b) Condition
+if (grepl("csv", anno_file)) {
+    anno <- read.csv(anno_file, header=TRUE, sep=",")
+} else {
+    anno <- read.table(anno_file, header=TRUE, sep="\t")
+}
 print(head(anno))
 rownames(anno) <- anno$sample
 p.threshold <- 0.05   ##define threshold for filtering
@@ -31,11 +41,6 @@ log2FC <- 1.58 # 3 fold change
 ### subset raw and conditional data for defined pairs
 anno <- anno[(anno$Condition ==firstC |anno$Condition ==SecondC),]
 rawcount <- rawcount[,names(rawcount) %in% anno$sample]
-
-# convert rawcount to numeric * not sure I need this
-#rawcount <- as.matrix(rawcount)
-#is.numeric(rawcount)
-#print(head(rawcount))
 
 ############################### Create DESeq2 datasets #############################
 dds <- DESeqDataSetFromMatrix(countData = rawcount, colData = anno, design = ~Condition )
@@ -52,7 +57,7 @@ res[, nam] <- as.logical(res$log2FoldChange < 0)
 genes.deseq <- row.names(res)[which(res$threshold)]
 genes_deseq2_sig <- res[which(res$threshold),]
 
-file <- paste(outdir, firstC,'_', SecondC, '.', Method, '.deseq2_results_padj0.05.csv', sep = '')
-all_results <- paste(outdir, firstC, '_', SecondC, '.', Method, '.deseq2_results_all.csv', sep = '')
+file <- paste(outdir, firstC,'_', SecondC, '.deseq2_results_padj0.05.csv', sep = '')
+all_results <- paste(outdir, firstC, '_', SecondC, '.deseq2_results_all.csv', sep = '')
 write.table(genes_deseq2_sig,file,sep = ",")
 write.table(res,all_results,sep = ",")
